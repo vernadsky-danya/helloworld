13e8e96fd119ef8af4a5f3d51ee0ec0152027878
vernadsky-danya <danya-sten151@ya.ru>
2016-11-07 16:40:24 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+public static Scanner in = new Scanner (System.in);
+public static PrintStream out =  System.out;
+    public static void main(String[] args) {
+	int a;
+        a = in.nextInt();
+int b= 2 << a-1;
+        out.println(b);
+    }
+}
